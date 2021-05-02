10dd5d39cbe78ee4bea3ed8e06ec5cc44dde4651
atoool <scrapycoco.sam@gmail.com>
2021-05-02 20:18:04 +0530

    integrated bottom tab bar and minor changes

--- src/routes/TabNavigator.js
+++ src/routes/TabNavigator.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {AddAPic, PicADays, Summary} from '../screens';
+import {BottomBar} from '../components';
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="PicADays" component={PicADays} />
+    <Tab.Navigator tabBar={props => <BottomBar {...props} />}>
+      <Tab.Screen name="PickADays" component={PicADays} />
       <Tab.Screen name="AddAPic" component={AddAPic} />
       <Tab.Screen name="Summary" component={Summary} />
     </Tab.Navigator>

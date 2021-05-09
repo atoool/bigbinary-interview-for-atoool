e2535b184428dbe2fad23d4fab53f3abe34c51a7
atoool <scrapycoco.sam@gmail.com>
2021-05-09 17:25:18 +0530

    api integrations

---
+++ src/api/API_URL.js
@@ -0,0 +1,2 @@
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export {API_URL};
--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,17 @@
+import axios from 'axios';
 import {ToastAndroid} from 'react-native';
-import {get} from './networkUtils';
+import {API_URL} from './API_URL';
 
 // Endpoints that Doesn't Require Authentication
-export const api = async (PAYLOAD, locale) => {
-  const URL = '/api';
-  return get(URL, PAYLOAD, false).catch(e => {
-    // console.log(e);
-    ToastAndroid.show(e, ToastAndroid.SHORT);
-  });
+export const getWeather = async PAYLOAD => {
+  const appid = '31e4f6cb2178d7d4ba1eb33368941c6f';
+  let PARAMS = `?appid=${appid}&q=${'kochi'}`;
+  return await axios
+    .get(API_URL + PARAMS)
+    .then(data => {
+      return data;
+    })
+    .catch(e => {
+      ToastAndroid.show(e, ToastAndroid.SHORT);
+    });
 };

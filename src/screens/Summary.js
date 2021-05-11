f7b3fadae39c60667216a0244df8b534fe8fd433
atoool <scrapycoco.sam@gmail.com>
2021-05-12 01:33:23 +0530

    summary functionalities done

--- src/screens/Summary.js
+++ src/screens/Summary.js
@@ -1,20 +1,78 @@
-import React from 'react';
+import moment from 'moment';
+import React, {useContext} from 'react';
 import {useEffect} from 'react';
 import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
 import {SummaryBox} from '../components';
+import {AppContext} from '../contexts';
 import {Colors} from '../styles';
 
 const Summary = () => {
+  const {data} = useContext(AppContext);
   useEffect(() => {
-    const onmount = async () => {};
-    onmount();
+    const onMount = async () => {};
+    onMount();
   }, []);
+
+  const recordedDays = data.length;
+
+  const getProgressData = () => {
+    if (recordedDays !== 0) {
+      const startedOn = data[recordedDays - 1]?.date;
+      const totalDays = moment().diff(moment(startedOn), 'days');
+      return `${recordedDays}/${totalDays}`;
+    }
+    return '0/0';
+  };
+
+  const getHighestTemp = () => {
+    if (recordedDays !== 0) {
+      highest = data[0].temperature;
+      lowest = highest;
+      data?.map(itm => {
+        if (itm?.temperature > highest) {
+          highest = itm?.temperature;
+          highestDay = moment(itm.date).format('ddd MMM DD, YYYY');
+        } else if (itm?.temperature < lowest) {
+          lowest = itm?.temperature;
+          lowestDay = moment(itm.date).format('ddd MMM DD, YYYY');
+        }
+      });
+      return {lowest, highest, lowestDay, highestDay};
+    }
+  };
+
+  let today = moment().format('ddd MMM DD, YYYY');
+  let {
+    lowest = 0,
+    highest = 0,
+    highestDay = today,
+    lowestDay = today,
+  } = getHighestTemp();
+
+  const dayData = {
+    title: 'Days',
+    mainText: getProgressData(),
+    subText: `You have recorded ${data.length} days since the first day`,
+  };
+
+  const highTempData = {
+    title: 'Hottest day',
+    mainText: `${highest}˚`,
+    subText: highestDay,
+  };
+
+  const lowTempData = {
+    title: 'Coldest day',
+    mainText: `${lowest}˚`,
+    subText: lowestDay,
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <SummaryBox data={data[0]} />
-        <SummaryBox data={data[1]} />
-        <SummaryBox data={data[2]} />
+        <SummaryBox data={dayData} />
+        <SummaryBox data={highTempData} />
+        <SummaryBox data={lowTempData} />
       </ScrollView>
     </SafeAreaView>
   );
@@ -28,23 +86,5 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
   },
 });
-
-const data = [
-  {
-    title: 'Days',
-    mainText: '17/19',
-    subText: 'You have recorded 17 days since the first day',
-  },
-  {
-    title: 'Hottest day',
-    mainText: '39˚',
-    subText: 'Sun Jan 12, 2021',
-  },
-  {
-    title: 'Coldest day',
-    mainText: '21˚',
-    subText: 'Mon Jan 1, 2021',
-  },
-];
 
 export default Summary;

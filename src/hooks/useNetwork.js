10e3bbf57338052911dae12d8be9d04d05313fe2
atoool <scrapycoco.sam@gmail.com>
2021-05-13 09:52:30 +0530

    netwrok check minor change

--- src/hooks/useNetwork.js
+++ src/hooks/useNetwork.js
@@ -5,7 +5,7 @@ import NetInfo from '@react-native-community/netinfo';
 const useNetwork = ({onChangeNetwork}) => {
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener(state => {
-      onChangeNetwork(!state?.isConnected);
+      onChangeNetwork(!state?.isInternetReachable);
     });
     return () => {
       unsubscribe();
